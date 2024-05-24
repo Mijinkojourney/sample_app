b95c53175461f57e8665bacbf4db4f277984e9be
Mijinkojourney <166620611+Mijinkojourney@users.noreply.github.com>
2024-05-24 11:58:21 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    ?user = User.find_by(email: params[:session][:email].downcase)
-    if ?user && ?user.authenticate(params[:session][:password])
-      log_in ?user
-      params[:session][:remember_me] == '1' ? remember(?user) : forget(?user)
-      redirect_to ?user
-      
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      forwarding_url = session[:forwarding_url]
+      reset_session
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      log_in user
+      redirect_to forwarding_url || user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
@@ -18,6 +19,6 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out if logged_in?
-      redirect_to root_url, status: :see_other
+    redirect_to root_url, status: :see_other
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,9 @@ module SessionsHelper
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
+       # セッションリプレイ攻撃から保護する
+    # 詳しくは https://techracho.bpsinc.jp/hachi8833/2023_06_02/130443 を参照
+    session[:session_token] = user.session_token
   end
 
   # ユーザーのセッションを永続的にする
@@ -12,18 +15,25 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # 記憶トークンcookieに対応するユーザーを返す
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.encrypted[:user_id])
-      raise       # テストがパスすれば、この部分がテストされていないことがわかる
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
+    # 記憶トークンのcookieに対応するユーザーを返す
+    def current_user
+      if (user_id = session[:user_id])
+        user = User.find_by(id: user_id)
+        if user && session[:session_token] == user.session_token
+          @current_user = user
+        end
+      elsif (user_id = cookies.encrypted[:user_id])
+        user = User.find_by(id: user_id)
+        if user && user.authenticated?(cookies[:remember_token])
+          log_in user
+          @current_user = user
+        end
       end
     end
+
+     # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
   end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
@@ -44,4 +54,9 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+    # アクセスしようとしたURLを保存する
+    def store_location
+      session[:forwarding_url] = request.original_url if request.get?
+    end
 end
